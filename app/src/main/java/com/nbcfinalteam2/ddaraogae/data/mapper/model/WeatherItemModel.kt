052701060c3436889cd18e09f0f42e5111a19489
neoneoneo123 <gouxrunner@gmail.com>
2024-06-03 20:33:54 +0900

    add: 날씨 mapper 생성

---
+++ app/src/main/java/com/nbcfinalteam2/ddaraogae/data/mapper/model/WeatherItemModel.kt
@@ -0,0 +1,8 @@
+package com.nbcfinalteam2.ddaraogae.data.mapper.model
+
+data class WeatherItemModel (
+    val id : Long,
+    val temperature: Double,
+    val pm25: Double,
+    val pm10: Double,
+)
