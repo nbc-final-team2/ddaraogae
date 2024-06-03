2dd19d6a9b7f328c6e26d1fed9b81aa93b8fb2c3
theBettor <be0128st@gmail.com>
2024-06-03 19:07:10 +0900

    design: 지도 띄우기
    issue#9 참고

--- app/src/main/java/com/nbcfinalteam2/ddaraogae/presentation/ui/walk/WalkFragment.kt
+++ app/src/main/java/com/nbcfinalteam2/ddaraogae/presentation/ui/walk/WalkFragment.kt
@@ -1,4 +1,84 @@
 package com.nbcfinalteam2.ddaraogae.presentation.ui.walk
 
-class WalkFragment {
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.naver.maps.map.LocationTrackingMode
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.util.FusedLocationSource
+import com.nbcfinalteam2.ddaraogae.R
+import com.nbcfinalteam2.ddaraogae.databinding.FragmentWalkBinding
+
+class WalkFragment : Fragment(), OnMapReadyCallback {
+    private val LOCATION_PERMISSION_REQUEST_CODE = 5000
+
+    private val PERMISSIONS = arrayOf(
+        android.Manifest.permission.ACCESS_FINE_LOCATION,
+        android.Manifest.permission.ACCESS_COARSE_LOCATION
+    )
+    private lateinit var binding: FragmentWalkBinding
+
+    private lateinit var naverMap: NaverMap
+    private lateinit var locationSource: FusedLocationSource
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (!hasPermission()) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                PERMISSIONS,
+                LOCATION_PERMISSION_REQUEST_CODE
+            )
+        } else {
+            initMapView()
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentWalkBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    private fun initMapView() {
+        val fm = childFragmentManager
+        val mapFragment = fm.findFragmentById(R.id.fragment_walk) as MapFragment?
+            ?: MapFragment.newInstance().also {
+                fm.beginTransaction().add(R.id.fragment_walk, it).commit()
+            }
+        // fragment의 getMapAsync() 메서드로 OnMapReadyCallback 콜백을 등록하면 비동기로 NaverMap 객체를 얻을 수 있다.
+        mapFragment.getMapAsync(this)
+        locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+    }
+
+    // hasPermission()에서는 위치 권한이 있을 경우 true를, 없을 경우 false를 반환한다.
+    private fun hasPermission(): Boolean {
+        for (permission in PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(requireActivity(), permission)
+                != PackageManager.PERMISSION_GRANTED
+            ) {
+                return false
+            }
+        }
+        return true
+    }
+
+    override fun onMapReady(naverMap: NaverMap) {
+        this.naverMap = naverMap
+        // 현재 위치
+        naverMap.locationSource = locationSource
+        // 현재 위치 버튼 기능
+        naverMap.uiSettings.isLocationButtonEnabled = true
+        // 위치를 추적하면서 카메라도 따라 움직인다.
+        naverMap.locationTrackingMode = LocationTrackingMode.Follow
+    }
 }
