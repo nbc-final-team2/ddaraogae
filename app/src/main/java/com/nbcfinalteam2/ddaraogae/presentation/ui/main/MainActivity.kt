0b23f4405da967b0f03cea295bae730c104b8e4e
neoneoneo123 <gouxrunner@gmail.com>
2024-06-04 19:02:04 +0900

    feat: 탭 선택시 화면 이동 구현

--- app/src/main/java/com/nbcfinalteam2/ddaraogae/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/nbcfinalteam2/ddaraogae/presentation/ui/main/MainActivity.kt
@@ -3,20 +3,24 @@ package com.nbcfinalteam2.ddaraogae.presentation.ui.main
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.nbcfinalteam2.ddaraogae.R
+import com.nbcfinalteam2.ddaraogae.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(binding.root)
 
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fcv_nav_container) as NavHostFragment
+        val navController = navHostFragment.findNavController()
+        binding.bnvNavBottom.setupWithNavController(navController)
     }
 }
