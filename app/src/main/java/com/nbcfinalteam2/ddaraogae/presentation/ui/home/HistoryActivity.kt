196ab353e0ae3e74833af26aa3101d75a453bb8d
Juseonghyun <wntjdgus99@naver.com>
2024-06-05 16:48:40 +0900

    feat: History에서 표시할 산책그래프를 위한 초기 뷰바인딩작업
    History에서 표시할 산책그래프를 위한 초기 뷰바인딩작업및 잘못 만든 EmptyActivity로 인한 AndroidManifest와 strings 업데이트 내역이있음
    
    issue: #26

--- app/src/main/java/com/nbcfinalteam2/ddaraogae/presentation/ui/home/HistoryActivity.kt
+++ app/src/main/java/com/nbcfinalteam2/ddaraogae/presentation/ui/home/HistoryActivity.kt
@@ -1,4 +1,26 @@
 package com.nbcfinalteam2.ddaraogae.presentation.ui.home
 
-class HistoryActivity {
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.nbcfinalteam2.ddaraogae.R
+import com.nbcfinalteam2.ddaraogae.databinding.ActivityHistoryBinding
+
+class HistoryActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHistoryBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityHistoryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
 }
