a5d553ed7495edebb91d2783e85045a197112a48
Juseonghyun <wntjdgus99@naver.com>
2024-06-06 21:50:47 +0900

    feat: Activity 기본 셋팅및 manifest등록
    Activity 기본 셋팅및 manifest등록
    
    issue: #40

--- app/src/main/java/com/nbcfinalteam2/ddaraogae/presentation/ui/home/AddActivity.kt
+++ app/src/main/java/com/nbcfinalteam2/ddaraogae/presentation/ui/home/AddActivity.kt
@@ -1,4 +1,24 @@
 package com.nbcfinalteam2.ddaraogae.presentation.ui.home
 
-class AddActivity {
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.nbcfinalteam2.ddaraogae.R
+import com.nbcfinalteam2.ddaraogae.databinding.ActivityAddBinding
+
+class AddActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAddBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
 }
