9af51f3510b43c6cfb931aaeab6840dea2feea47
neoneoneo123 <gouxrunner@gmail.com>
2024-06-07 19:49:48 +0900

    test: test용 코드 수정 및 추가 - TestViewModel 추가 - MainActivity에서 viewmodel을 사용하도록 수정

--- app/src/main/java/com/nbcfinalteam2/ddaraogae/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/nbcfinalteam2/ddaraogae/presentation/ui/main/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.nbcfinalteam2.ddaraogae.presentation.ui.main
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.nbcfinalteam2.ddaraogae.R
 import com.nbcfinalteam2.ddaraogae.databinding.ActivityMainBinding
-import com.nbcfinalteam2.ddaraogae.domain.repository.StoreRepository
+import com.nbcfinalteam2.ddaraogae.domain.usecase.GetStoreDataUseCase
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -19,7 +21,7 @@ import javax.inject.Inject
 /**
  * 업체 검색 test code를 사용하기 위해서는 아래 EntryPoint code 주석을 해지해야 합니다.
  */
-//@AndroidEntryPoint
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
@@ -33,6 +35,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         initBottomNavigation()
+        testApiCall() //test fun
     }
 
     private fun initBottomNavigation() {
@@ -44,17 +47,18 @@ class MainActivity : AppCompatActivity() {
     /**
      * 업체 검색 test code 입니다.
      * 필요시 주석을 해재하여 사용하시고, 사용 후에는 삭제해주시면 되겠습니다.
+     * 삭제 시 TestViewModel도 함께 삭제해주셔야 합니다.
      */
-//    @Inject
-//    lateinit var repository: StoreRepository
-//    private fun testApiCall() {
-//        CoroutineScope(Dispatchers.Main).launch {
-//            try {
-//                val storeData = repository.getStoreData("37.5560294", "127.0872547")
-//                Log.d("MainActivity", "store data: $storeData")
-//            } catch (e: Exception) {
-//                Log.e("MainActivity", "Error fetching store data: ${e.message}")
-//            }
-//        }
-//    }
+    private val viewModel: TestViewModel by viewModels()
+    private fun testApiCall() {
+        val lat = "" //위도 lat
+        val lng = "" //경도 lng
+        viewModel.fetchStoreData(lat, lng)
+
+        viewModel.storeData.observe(this) {
+            val data = it
+            Log.d("MainActivity", "store data: $data")
+
+        }
+    }
 }
