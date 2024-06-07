65f72fa6630e5710229afaea19fcfb24c620d39b
neoneoneo123 <gouxrunner@gmail.com>
2024-06-07 21:30:16 +0900

    refactor: entity에 id 추가 및 id를 기준으로 중복 데이터를 제거한 리스트를 반환하도록 수정

--- app/src/main/java/com/nbcfinalteam2/ddaraogae/data/mapper/StoreMapper.kt
+++ app/src/main/java/com/nbcfinalteam2/ddaraogae/data/mapper/StoreMapper.kt
@@ -7,16 +7,19 @@ object StoreMapper {
     fun toStoreData(storeResponseForHospital: Store, storeResponseForFood: Store): List<StoreEntity> {
         val resultForHospital = storeResponseForHospital.documents.map {
             StoreEntity(
+                id = it.id,
                 placeName = it.place_name,
                 categoryGroupName = it.category_group_name ?: "none",
                 address = it.road_address_name,
                 phone = it.phone,
                 lat = it.y,
                 lng = it.x
+                //id 추가
             )
         }
         val resultForFood = storeResponseForFood.documents.map {
             StoreEntity(
+                id = it.id,
                 placeName = it.place_name,
                 categoryGroupName = it.category_group_name ?: "none",
                 address = it.road_address_name,
@@ -25,7 +28,7 @@ object StoreMapper {
                 lng = it.x
             )
         }
-        val combinedList = (resultForHospital + resultForFood).toSet().toList()
+        val combinedList = (resultForHospital + resultForFood).distinctBy { it.id }
 
         return combinedList
     }
--- app/src/main/java/com/nbcfinalteam2/ddaraogae/domain/entity/StoreEntity.kt
+++ app/src/main/java/com/nbcfinalteam2/ddaraogae/domain/entity/StoreEntity.kt
@@ -1,6 +1,7 @@
 package com.nbcfinalteam2.ddaraogae.domain.entity
 
 data class StoreEntity (
+    val id: String,
     val placeName: String,
     val categoryGroupName: String,
     val address: String,
