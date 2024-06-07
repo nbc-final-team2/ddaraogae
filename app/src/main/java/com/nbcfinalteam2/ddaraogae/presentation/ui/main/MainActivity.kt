d4309e91bba52b1423b2bb165a3c763f48888de5
neoneoneo123 <gouxrunner@gmail.com>
2024-06-07 19:21:50 +0900

    remove: 불필요한 import 삭제

--- app/src/main/java/com/nbcfinalteam2/ddaraogae/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/nbcfinalteam2/ddaraogae/presentation/ui/main/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.nbcfinalteam2.ddaraogae.presentation.ui.main
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
