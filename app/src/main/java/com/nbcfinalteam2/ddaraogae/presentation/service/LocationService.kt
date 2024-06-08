2b5e05df245c30a210cc589ebd27e0e109f24340
Lee HuiGwang <lhk9805@gmail.com>
2024-06-09 04:16:13 +0900

    add: Service 코드 추가
    issue: #52

---
+++ app/src/main/java/com/nbcfinalteam2/ddaraogae/presentation/service/LocationService.kt
@@ -0,0 +1,21 @@
+package com.nbcfinalteam2.ddaraogae.presentation.service
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class LocationService: Service() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+
+        return START_STICKY
+    }
+
+    override fun onBind(p0: Intent?): IBinder? {
+        return null
+    }
+}
