576ac2c06787bbbb1824c466f08704aad3655789
neoneoneo123 <gouxrunner@gmail.com>
2024-06-10 15:24:04 +0900

    fix : OO구, OO군의 이름이 city 데이터가 되도록 수정

--- app/src/main/java/com/nbcfinalteam2/ddaraogae/data/mapper/WeatherMapper.kt
+++ app/src/main/java/com/nbcfinalteam2/ddaraogae/data/mapper/WeatherMapper.kt
@@ -1,5 +1,6 @@
 package com.nbcfinalteam2.ddaraogae.data.mapper
 
+import android.util.Log
 import com.nbcfinalteam2.ddaraogae.data.dto.CityDto
 import com.nbcfinalteam2.ddaraogae.data.dto.DustDto
 import com.nbcfinalteam2.ddaraogae.data.dto.WeatherDto
@@ -7,7 +8,11 @@ import com.nbcfinalteam2.ddaraogae.domain.entity.WeatherEntity
 
 object WeatherMapper {
     fun toWeatherData(weather: WeatherDto, dust: DustDto, address: CityDto) : WeatherEntity {
-        val cityData = address.documents[0].region3depthName
+        val cityData = if (address.documents[0].region2depthName.isNotEmpty()) {
+            address.documents[0].region2depthName
+        } else {
+            address.documents[1].region2depthName
+        }
 
         val weatherData = WeatherEntity(
             id = weather.weather[0].id,
