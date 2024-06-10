bd15a0a918741ed17148d6bbb5e54fbcb5f7e487
neoneoneo123 <gouxrunner@gmail.com>
2024-06-10 18:48:55 +0900

    fix: cityData를 할당하는 과정에서 null값이 안전하게 처리되도록 수정

--- app/src/main/java/com/nbcfinalteam2/ddaraogae/data/mapper/WeatherMapper.kt
+++ app/src/main/java/com/nbcfinalteam2/ddaraogae/data/mapper/WeatherMapper.kt
@@ -7,10 +7,12 @@ import com.nbcfinalteam2.ddaraogae.domain.entity.WeatherEntity
 
 object WeatherMapper {
     fun toWeatherData(weather: WeatherDto, dust: DustDto, address: CityDto) : WeatherEntity {
-        val cityData = if (address.documents?.get(0)?.region2depthName.isNullOrEmpty()) {
-            address.documents?.get(0)?.region2depthName
-        } else {
-            address.documents?.get(1)?.region2depthName
+        val cityData = address.documents?.let { docs ->
+            if (docs.isNotEmpty() && docs[0].region2depthName.isNullOrEmpty()) {
+                docs.getOrNull(1)?.region2depthName
+            } else {
+                docs.getOrNull(0)?.region2depthName
+            }
         }
 
         val weatherData = WeatherEntity(
