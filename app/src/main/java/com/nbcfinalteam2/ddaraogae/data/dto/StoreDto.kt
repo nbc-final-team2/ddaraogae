c061f60b4e7efe25b67d9e74f57a1b1be35e1091
neoneoneo123 <gouxrunner@gmail.com>
2024-06-10 11:15:50 +0900

    fix: StoreDto에 사용되는 변수 타입을 nullable로 변경

--- app/src/main/java/com/nbcfinalteam2/ddaraogae/data/dto/StoreDto.kt
+++ app/src/main/java/com/nbcfinalteam2/ddaraogae/data/dto/StoreDto.kt
@@ -3,30 +3,30 @@ package com.nbcfinalteam2.ddaraogae.data.dto
 import com.google.gson.annotations.SerializedName
 
 data class StoreDto(
-    @SerializedName("documents") val documents: List<Document>,
-    @SerializedName("meta") val meta: Meta,
+    @SerializedName("documents") val documents: List<Document>?,
+    @SerializedName("meta") val meta: Meta?,
 )
 
 data class Document(
-    @SerializedName("address_name") val address_name: String,
+    @SerializedName("address_name") val address_name: String?,
     @SerializedName("category_group_code") val category_group_code: String?,
     @SerializedName("category_group_name") val category_group_name: String?,
-    @SerializedName("category_name") val category_name: String,
-    @SerializedName("distance") val distance: String,
-    @SerializedName("id") val id: String,
-    @SerializedName("phone") val phone: String,
-    @SerializedName("place_name") val place_name: String,
-    @SerializedName("place_url") val place_url: String,
-    @SerializedName("road_address_name") val road_address_name: String,
-    @SerializedName("x") val x: String, //longitude
-    @SerializedName("y") val y: String  //latitude
+    @SerializedName("category_name") val category_name: String?,
+    @SerializedName("distance") val distance: String?,
+    @SerializedName("id") val id: String?,
+    @SerializedName("phone") val phone: String?,
+    @SerializedName("place_name") val place_name: String?,
+    @SerializedName("place_url") val place_url: String?,
+    @SerializedName("road_address_name") val road_address_name: String?,
+    @SerializedName("x") val x: String?, //longitude
+    @SerializedName("y") val y: String? //latitude
 )
 
 data class Meta(
-    @SerializedName("is_end") val is_end: Boolean,
-    @SerializedName("pageable_count") val pageable_count: Int,
-    @SerializedName("same_name") val same_name: SameName,
-    @SerializedName("total_count") val total_count: Int,
+    @SerializedName("is_end") val is_end: Boolean?,
+    @SerializedName("pageable_count") val pageable_count: Int?,
+    @SerializedName("same_name") val same_name: SameName?,
+    @SerializedName("total_count") val total_count: Int?,
 )
 
 data class SameName(
