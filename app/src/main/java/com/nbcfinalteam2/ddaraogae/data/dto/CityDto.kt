25ee7bf78f9ef6f8b693e403016d27f28747b7d2
neoneoneo123 <gouxrunner@gmail.com>
2024-06-10 10:38:26 +0900

    fix: 변수명이 카멜케이스로 변경

--- app/src/main/java/com/nbcfinalteam2/ddaraogae/data/dto/CityDto.kt
+++ app/src/main/java/com/nbcfinalteam2/ddaraogae/data/dto/CityDto.kt
@@ -7,9 +7,9 @@ data class City(
 )
 
 data class Address(
-    @SerializedName("region_1depth_name") val region_1depth_name: String,
-    @SerializedName("region_2depth_name") val region_2depth_name: String,
-    @SerializedName("region_3depth_name") val region_3depth_name: String,
+    @SerializedName("region_1depth_name") val region1depthName: String,
+    @SerializedName("region_2depth_name") val region2depthName: String,
+    @SerializedName("region_3depth_name") val region3depthName: String,
     @SerializedName("x") val x: Double,
     @SerializedName("y") val y: Double,
 )
