db1260f7f5e0f54a54b721c72741b6aa4e7a4ebb
neoneoneo123 <gouxrunner@gmail.com>
2024-06-10 11:09:50 +0900

    fix: 변수 타입을 nullable로 변경

--- app/src/main/java/com/nbcfinalteam2/ddaraogae/data/dto/CityDto.kt
+++ app/src/main/java/com/nbcfinalteam2/ddaraogae/data/dto/CityDto.kt
@@ -7,9 +7,9 @@ data class CityDto(
 )
 
 data class Address(
-    @SerializedName("region_1depth_name") val region1depthName: String,
-    @SerializedName("region_2depth_name") val region2depthName: String,
-    @SerializedName("region_3depth_name") val region3depthName: String,
-    @SerializedName("x") val x: Double,
-    @SerializedName("y") val y: Double,
+    @SerializedName("region_1depth_name") val region1depthName: String?,
+    @SerializedName("region_2depth_name") val region2depthName: String?,
+    @SerializedName("region_3depth_name") val region3depthName: String?,
+    @SerializedName("x") val x: Double?,
+    @SerializedName("y") val y: Double?,
 )
