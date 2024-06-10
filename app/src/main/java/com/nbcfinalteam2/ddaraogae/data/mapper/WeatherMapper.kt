1be1d66ec37b8496c9ae8252d30de01448e56239
neoneoneo123 <gouxrunner@gmail.com>
2024-06-10 12:32:18 +0900

    fix : 관련된 모든 변수 nullable 타입으로 변경

--- app/src/main/java/com/nbcfinalteam2/ddaraogae/data/mapper/WeatherMapper.kt
+++ app/src/main/java/com/nbcfinalteam2/ddaraogae/data/mapper/WeatherMapper.kt
@@ -7,15 +7,15 @@ import com.nbcfinalteam2.ddaraogae.domain.entity.WeatherEntity
 
 object WeatherMapper {
     fun toWeatherData(weather: WeatherDto, dust: DustDto, address: CityDto) : WeatherEntity {
-        var addressData = address.documents?.firstOrNull()
+        var addressData = address.documents?.get(0)
         if (addressData == null) { addressData = address.documents?.get(1) }
-        val cityData = addressData?.region3depthName ?: addressData?.region2depthName ?: addressData?.region1depthName ?: ""
+        val cityData = addressData?.region3depthName ?: addressData?.region2depthName ?: addressData?.region1depthName
 
         val weatherData = WeatherEntity(
-            id = weather.weather?.firstOrNull()?.id ?: 0L,
-            temperature = weather.main?.temp ?: 0.0,
-            pm10 = dust.list?.firstOrNull()?.components?.pm10 ?: 0.0,
-            pm25 = dust.list?.firstOrNull()?.components?.pm25 ?: 0.0,
+            id = weather.weather?.firstOrNull()?.id,
+            temperature = weather.main?.temp,
+            pm10 = dust.list?.firstOrNull()?.components?.pm10,
+            pm25 = dust.list?.firstOrNull()?.components?.pm25,
             city = cityData
         )
 
