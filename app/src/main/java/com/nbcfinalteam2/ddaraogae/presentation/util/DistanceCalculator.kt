f4f2354e84d49c31c5aa1624669373379517042f
neoneoneo123 <gouxrunner@gmail.com>
2024-06-10 20:38:22 +0900

    add: 좌표 간 거리 계산 object 추가

---
+++ app/src/main/java/com/nbcfinalteam2/ddaraogae/presentation/util/DistanceCalculator.kt
@@ -0,0 +1,17 @@
+package com.nbcfinalteam2.ddaraogae.presentation.util
+
+import android.location.Location
+
+object DistanceCalculator {
+    fun getDistance(lat1: Double, lng1: Double, lat2: Double, lng2: Double) : Float {
+        val targetFrom = Location("from")
+        targetFrom.latitude = lat1
+        targetFrom.longitude = lng1
+
+        val targetTo = Location("to")
+        targetTo.latitude = lat2
+        targetTo.longitude = lng2
+
+        return targetFrom.distanceTo(targetTo)
+    }
+}
