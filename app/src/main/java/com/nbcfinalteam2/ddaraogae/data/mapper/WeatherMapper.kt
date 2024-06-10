f64f5f2186a52e6226ced71a2c3702954d88c413
neoneoneo123 <gouxrunner@gmail.com>
2024-06-10 17:32:45 +0900

    fix: 사용되는 모든 변수 nullable하게 변경

--- app/src/main/java/com/nbcfinalteam2/ddaraogae/data/mapper/WeatherMapper.kt
+++ app/src/main/java/com/nbcfinalteam2/ddaraogae/data/mapper/WeatherMapper.kt
@@ -1,6 +1,5 @@
 package com.nbcfinalteam2.ddaraogae.data.mapper
 
-import android.util.Log
 import com.nbcfinalteam2.ddaraogae.data.dto.CityDto
 import com.nbcfinalteam2.ddaraogae.data.dto.DustDto
 import com.nbcfinalteam2.ddaraogae.data.dto.WeatherDto
@@ -8,17 +7,17 @@ import com.nbcfinalteam2.ddaraogae.domain.entity.WeatherEntity
 
 object WeatherMapper {
     fun toWeatherData(weather: WeatherDto, dust: DustDto, address: CityDto) : WeatherEntity {
-        val cityData = if (address.documents[0].region2depthName.isNotEmpty()) {
-            address.documents[0].region2depthName
+        val cityData = if (address.documents?.get(0)?.region2depthName.isNullOrEmpty()) {
+            address.documents?.get(0)?.region2depthName
         } else {
-            address.documents[1].region2depthName
+            address.documents?.get(1)?.region2depthName
         }
 
         val weatherData = WeatherEntity(
-            id = weather.weather[0].id,
-            temperature = weather.main.temp,
-            pm10 = dust.list[0].components.pm10,
-            pm25 = dust.list[0].components.pm25,
+            id = weather.weather?.get(0)?.id,
+            temperature = weather.main?.temp,
+            pm10 = dust.list?.get(0)?.components?.pm10,
+            pm25 = dust.list?.get(0)?.components?.pm25,
             city = cityData
         )
 
--- app/src/main/java/com/nbcfinalteam2/ddaraogae/domain/entity/WeatherEntity.kt
+++ app/src/main/java/com/nbcfinalteam2/ddaraogae/domain/entity/WeatherEntity.kt
@@ -1,9 +1,9 @@
 package com.nbcfinalteam2.ddaraogae.domain.entity
 
 data class WeatherEntity (
-    val id : Long,
-    val temperature: Double,
-    val pm25: Double,
-    val pm10: Double,
-    val city: String,
+    val id : Long?,
+    val temperature: Double?,
+    val pm25: Double?,
+    val pm10: Double?,
+    val city: String?,
 )
