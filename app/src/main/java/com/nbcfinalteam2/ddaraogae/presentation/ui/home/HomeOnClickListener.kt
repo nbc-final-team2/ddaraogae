70beb7f464ee36cda909393867393b65ed14d122
Juseonghyun <wntjdgus99@naver.com>
2024-06-11 22:03:13 +0900

    remove: 안쓰는 Interface삭제
    코드 리팩토링으로 인해 안쓰는 Interface삭제
    
    issue: #40

--- app/src/main/java/com/nbcfinalteam2/ddaraogae/presentation/ui/home/HomeOnClickListener.kt
+++
@@ -1,8 +0,0 @@
-package com.nbcfinalteam2.ddaraogae.presentation.ui.home
-
-import com.nbcfinalteam2.ddaraogae.presentation.model.DogInfo
-
-interface HomeOnClickListener {
-    fun onAddClick()
-    fun onDogClick(dogData: DogInfo)
-}
