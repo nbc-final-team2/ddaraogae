cecef4a05e9b73138370007fe2f5119a8c3ee06c
Juseonghyun <wntjdgus99@naver.com>
2024-06-11 14:44:18 +0900

    rename: walkInfo -> WalkingInfo 로 네이밍 변경및 dataclass 내용물 수정
    walkInfo -> WalkingInfo 로 네이밍 변경및 dataclass 내용물 수정
    
    issue: #40

--- app/src/main/java/com/nbcfinalteam2/ddaraogae/presentation/model/WalkInfo.kt
+++
@@ -1,9 +0,0 @@
-package com.nbcfinalteam2.ddaraogae.presentation.model
-
-data class WalkInfo (
-    val id : Long?,
-    val temperature: Double?,
-    val pm25: Double?,
-    val pm10: Double?,
-    val city: String?,
-)
