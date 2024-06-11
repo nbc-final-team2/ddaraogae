37c26f2f53ad31e9aefcfec44a4fdb073b19d546
Juseonghyun <wntjdgus99@naver.com>
2024-06-11 14:42:21 +0900

    add: presentaion을 위한 WalkInfo 생성
    presentaion을 위한 모델 WalkInfo 생성
    
    issue: #40

---
+++ app/src/main/java/com/nbcfinalteam2/ddaraogae/presentation/model/WalkInfo.kt
@@ -0,0 +1,9 @@
+package com.nbcfinalteam2.ddaraogae.presentation.model
+
+data class WalkInfo (
+    val id : Long?,
+    val temperature: Double?,
+    val pm25: Double?,
+    val pm10: Double?,
+    val city: String?,
+)
