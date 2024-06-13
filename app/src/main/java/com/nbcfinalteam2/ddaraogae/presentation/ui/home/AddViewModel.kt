e6521caa6934dea5553c0b5fe6a0843064794768
Juseonghyun <wntjdgus99@naver.com>
2024-06-13 19:28:02 +0900

    remove: 예윤님과 task가 겹쳐 add코드 전부 삭제
    예윤님과 task가 겹쳐 add코드 전부 삭제
    
    issue: #40

--- app/src/main/java/com/nbcfinalteam2/ddaraogae/presentation/ui/home/AddViewModel.kt
+++ app/src/main/java/com/nbcfinalteam2/ddaraogae/presentation/ui/home/AddViewModel.kt
@@ -1,37 +1,5 @@
 package com.nbcfinalteam2.ddaraogae.presentation.ui.home
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.nbcfinalteam2.ddaraogae.domain.entity.DogEntity
-import com.nbcfinalteam2.ddaraogae.domain.usecase.GetCurrentUserUseCase
-import com.nbcfinalteam2.ddaraogae.domain.usecase.InsertDogUseCase
-import com.nbcfinalteam2.ddaraogae.presentation.model.DogInfo
-import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
-import javax.inject.Inject
+class AddViewModel {
 
-@HiltViewModel
-class AddViewModel @Inject constructor(
-    private val insertDogUseCase: InsertDogUseCase,
-    private val getCurrentUserUseCase: GetCurrentUserUseCase
-) : ViewModel() {
-
-    // 네트워크 예외처리, try catch가 좋을것같긴함
-//    fun addDog(dogInfo: DogInfo) {
-//        viewModelScope.launch {
-//            val user = getCurrentUserUseCase()
-//            user?.let {
-//                val dogEntity = DogEntity(
-//                    id = dogInfo.id,
-//                    name = dogInfo.name,
-//                    gender = dogInfo.gender,
-//                    age = dogInfo.age,
-//                    lineage = dogInfo.lineage,
-//                    memo = dogInfo.memo,
-//                    thumbnailUrl = dogInfo.thumbnailUrl
-//                )
-//                insertDogUseCase(dogEntity) // 네트워크 예외처리
-//            }
-//        }
-//    }
 }
