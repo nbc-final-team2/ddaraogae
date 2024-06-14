7b5ec25f83858a65761d28d2df2374251d2fa14b
Juseonghyun <wntjdgus99@naver.com>
2024-06-15 04:17:15 +0900

    feat: 다이얼로그 기본 셋팅완료
    다이얼로그 기본 셋팅완료
    
    issue: #118

--- app/src/main/java/com/nbcfinalteam2/ddaraogae/presentation/ui/home/WalkHistoryMapDialog.kt
+++ app/src/main/java/com/nbcfinalteam2/ddaraogae/presentation/ui/home/WalkHistoryMapDialog.kt
@@ -1,5 +1,31 @@
 package com.nbcfinalteam2.ddaraogae.presentation.ui.home
 
-//class WalkHistoryMapDialog : CalendarDialog() {
-//    private var _binding
-//}
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.nbcfinalteam2.ddaraogae.databinding.DialogWalkHistoryMapBinding
+
+class WalkHistoryMapDialog : DialogFragment() {
+    private var _binding: DialogWalkHistoryMapBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = DialogWalkHistoryMapBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
