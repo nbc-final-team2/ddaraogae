811daffdbfd2d535261be03e98029cf4058a5772
Lee HuiGwang <lhk9805@gmail.com>
2024-06-14 19:36:26 +0900

    feat: 회원탈퇴 시, 유저가 생성한 데이터 삭제하도록 변경
    - dogDelete쪽 await() 누락된 부분도 함께 수정
    
    issus: #115

--- app/src/main/java/com/nbcfinalteam2/ddaraogae/data/repository/AuthRepositoryImpl.kt
+++ app/src/main/java/com/nbcfinalteam2/ddaraogae/data/repository/AuthRepositoryImpl.kt
@@ -2,6 +2,9 @@ package com.nbcfinalteam2.ddaraogae.data.repository
 
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.nbcfinalteam2.ddaraogae.data.datasource.remote.firebase.FirebaseDataSourceImpl
 import com.nbcfinalteam2.ddaraogae.domain.entity.EmailAuthEntity
 import com.nbcfinalteam2.ddaraogae.domain.entity.UserEntity
 import com.nbcfinalteam2.ddaraogae.domain.repository.AuthRepository
@@ -9,7 +12,9 @@ import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
 class AuthRepositoryImpl @Inject constructor(
-    private val firebaseAuth: FirebaseAuth
+    private val firebaseAuth: FirebaseAuth,
+    private val firebaseFs: FirebaseFirestore,
+    private val fbStorage: FirebaseStorage
 ): AuthRepository {
 
     override suspend fun signInWithEmail(emailAuthEntity: EmailAuthEntity): Boolean {
@@ -29,6 +34,11 @@ class AuthRepositoryImpl @Inject constructor(
     }
 
     override suspend fun deleteAccount() {
+        val uid = firebaseAuth.currentUser?.uid ?: throw Exception("USER NOT EXIST")
+
+        firebaseFs.collection(PATH_USERDATA).document(uid).delete().await()
+        val deleteRef = fbStorage.reference.child("${PATH_USERDATA}/$uid")
+        deleteRef.delete().await()
         firebaseAuth.currentUser?.delete()?.await()
     }
 
@@ -51,5 +61,9 @@ class AuthRepositoryImpl @Inject constructor(
     override suspend fun isCurrentUserEmailVerified(): Boolean {
         firebaseAuth.currentUser?.reload()?.await()
         return firebaseAuth.currentUser?.isEmailVerified ?: false
+    }
+
+    companion object {
+        private const val PATH_USERDATA = "userData"
     }
 }
