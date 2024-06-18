c9af9b5bc43107e653534882178c81494378ae96
Lee HuiGwang <lhk9805@gmail.com>
2024-06-18 18:24:38 +0900

    refactor: Hilt 모듈 내 누락 어노테이션 추가 및 반려견 데이터 정렬 추가
    issue: #161

--- app/src/main/java/com/nbcfinalteam2/ddaraogae/app/di/FirebaseModule.kt
+++ app/src/main/java/com/nbcfinalteam2/ddaraogae/app/di/FirebaseModule.kt
@@ -12,17 +12,21 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object FirebaseModule {
 
     @Provides
+    @Singleton
     fun provideFirebaseFirestore(): FirebaseFirestore = Firebase.firestore
 
     @Provides
+    @Singleton
     fun provideFirebaseAuth(): FirebaseAuth = Firebase.auth
 
     @Provides
+    @Singleton
     fun provideFirebaseStorage(): FirebaseStorage = Firebase.storage
 }
