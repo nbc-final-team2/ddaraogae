e4840d51589f62449cf13d3cdb52d4888e2263e7
Lee HuiGwang <lhk9805@gmail.com>
2024-06-18 18:36:53 +0900

    remove: 불필요 파일 삭제
    issue: #161

--- app/src/main/java/com/nbcfinalteam2/ddaraogae/presentation/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/nbcfinalteam2/ddaraogae/presentation/ui/home/HomeViewModel.kt
@@ -20,7 +20,6 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-
 class HomeViewModel @Inject constructor(
     private val getDogListUseCase: GetDogListUseCase,
     private val getWalkingListByDogIdAndPeriodUseCase: GetWalkingListByDogIdAndPeriodUseCase,
