60a33c08b6d50a97d85e0482d4c21336051cd5d8
Juseonghyun <wntjdgus99@naver.com>
2024-06-23 18:23:48 +0900

    feat: 함수를 주석 처리한거 풀면서 bg 적용 완료
    issue: #202

--- app/src/main/java/com/nbcfinalteam2/ddaraogae/presentation/ui/home/HomeFragment.kt
+++ app/src/main/java/com/nbcfinalteam2/ddaraogae/presentation/ui/home/HomeFragment.kt
@@ -96,7 +96,7 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//        changeForAppStatusBar()
+        changeForAppStatusBar()
         setupWalkGraphForEmptyData()
         setupListener()
         setupAdapter()
