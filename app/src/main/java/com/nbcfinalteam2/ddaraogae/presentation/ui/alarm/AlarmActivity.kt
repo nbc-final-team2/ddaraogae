059893d66aad4f598e393d0e6d854e46f0b6b723
Lee HuiGwang <lhk9805@gmail.com>
2024-06-26 04:00:46 +0900

    design: AlarmActivity 디자인
    issue: #234

---
+++ app/src/main/java/com/nbcfinalteam2/ddaraogae/presentation/ui/alarm/AlarmActivity.kt
@@ -0,0 +1,11 @@
+package com.nbcfinalteam2.ddaraogae.presentation.ui.alarm
+
+import android.app.Activity
+import android.os.Bundle
+
+class AlarmActivity: Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
