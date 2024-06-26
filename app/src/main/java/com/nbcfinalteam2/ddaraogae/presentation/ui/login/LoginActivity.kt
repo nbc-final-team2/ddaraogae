4005714af2284f13220d31d0f1bbb14baf564476
JYeyun <117142316+JYeyun@users.noreply.github.com>
2024-06-26 19:02:18 +0900

    [fix]splash 테마 미 적용 에러 해결

--- app/src/main/java/com/nbcfinalteam2/ddaraogae/presentation/ui/login/LoginActivity.kt
+++ app/src/main/java/com/nbcfinalteam2/ddaraogae/presentation/ui/login/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.nbcfinalteam2.ddaraogae.presentation.ui.login
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.nbcfinalteam2.ddaraogae.R
 import com.nbcfinalteam2.ddaraogae.databinding.ActivityLoginBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -10,6 +11,9 @@ import dagger.hilt.android.AndroidEntryPoint
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding:ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen().apply {
+            setKeepOnScreenCondition { true }
+        }
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
 
