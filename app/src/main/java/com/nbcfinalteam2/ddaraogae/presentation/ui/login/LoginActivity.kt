bb61f4d630e19cf474a9490df0d9b25a60cd7a1d
JYeyun <117142316+JYeyun@users.noreply.github.com>
2024-06-27 10:02:27 +0900

    [fix] 스플래쉬 화면에서 안넘어가는 에러 해결

--- app/src/main/java/com/nbcfinalteam2/ddaraogae/presentation/ui/login/LoginActivity.kt
+++ app/src/main/java/com/nbcfinalteam2/ddaraogae/presentation/ui/login/LoginActivity.kt
@@ -1,8 +1,10 @@
 package com.nbcfinalteam2.ddaraogae.presentation.ui.login
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.fragment.app.viewModels
 import com.nbcfinalteam2.ddaraogae.R
 import com.nbcfinalteam2.ddaraogae.databinding.ActivityLoginBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -10,13 +12,15 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding:ActivityLoginBinding
+    private val viewModel: LoginViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        installSplashScreen().apply {
-            setKeepOnScreenCondition { true }
-        }
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
-
+//        splashScreen.setKeepOnScreenCondition{
+//            false
+//        }
         setFragment()
         setContentView(binding.root)
     }
