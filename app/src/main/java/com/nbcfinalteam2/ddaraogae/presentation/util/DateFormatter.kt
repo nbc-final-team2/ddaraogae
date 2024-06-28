33a2908ae53bd2d44de69a23624820f8cbe43a52
theBettor <be0128st@gmail.com>
2024-06-28 12:58:01 +0900

    refactor: 1. String -> Int
    string인 상황에서 strings.xml을 사용하려고 getString을 사용하려면 context가 필요했는데 이러면 viewmodeleh contentResolver?가 필요하다 하고 fragment도 수정해주어야 하는 것 같았다. 그래서 getString을 사용하지 않고 Int 타입으로 변경했다.

--- app/src/main/java/com/nbcfinalteam2/ddaraogae/presentation/util/DateFormatter.kt
+++ app/src/main/java/com/nbcfinalteam2/ddaraogae/presentation/util/DateFormatter.kt
@@ -88,28 +88,17 @@ object DateFormatter {
         return format.format(walkEndTime)
     }
 
-    fun getCurrentTimeAgo(date: Date?): String {
-        if (date == null) {
-            return "최근에 산책한 기록이 없어요!" // 시간에 대한 정보가 없을때는 아무것도 보여주지 않기 위함.
-        }
+    fun getCurrentTimeAgo(date: Date?): Int? {
+        if (date == null) return null
+
         val now: Long = System.currentTimeMillis()
-        val nowDate = Date(now) // 현재 시간을 Date 타입으로 변환
-        val timeDifference = nowDate.time - date.time // 현재 시간 - 특정 시간
+        val nowDate = Date(now)
+        val timeDifference = nowDate.time - date.time
 
         val seconds = timeDifference / 1000
         val minutes = seconds / 60
         val hours = minutes / 60
 
-        return when {
-            hours < 1 -> {
-                "금방 산책을 다녀왔어요!"
-            }
-            hours < 24 -> {
-                "$hours 시간 전"
-            }
-            else -> {
-                "산책한지 24시간이 넘었습니다"
-            }
-        }
+        return hours.toInt()
     }
 }
