d3eb7ca4908bd1a3c890ecd57183feec4034bbe2
neoneoneo123 <gouxrunner@gmail.com>
2024-07-02 13:50:34 +0900

    feat: 개인정보처리 동의서 상 현재 날짜 표기 - html text로 읽도록 처리함
    issue:269

--- app/src/main/java/com/nbcfinalteam2/ddaraogae/presentation/ui/mypage/MypageAgreementPrivacy.kt
+++ app/src/main/java/com/nbcfinalteam2/ddaraogae/presentation/ui/mypage/MypageAgreementPrivacy.kt
@@ -1,12 +1,18 @@
 package com.nbcfinalteam2.ddaraogae.presentation.ui.mypage
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.text.HtmlCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updatePadding
+import com.nbcfinalteam2.ddaraogae.R
 import com.nbcfinalteam2.ddaraogae.databinding.ActivityAgreementPrivacyBinding
+import com.nbcfinalteam2.ddaraogae.presentation.util.TextConverter.dateDateToString
+import okio.FileNotFoundException
+import java.util.Date
 
 class MypageAgreementPrivacy : AppCompatActivity() {
     private lateinit var binding: ActivityAgreementPrivacyBinding
@@ -24,6 +30,20 @@ class MypageAgreementPrivacy : AppCompatActivity() {
             val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
             view.updatePadding(insets.left, insets.top, insets.right, insets.bottom)
             WindowInsetsCompat.CONSUMED
+        }
+
+        try {
+            val assetManager = this.assets.open("doc_privacy_agreement")
+            val htmlText = assetManager.bufferedReader().use { it.readText() }
+            val spannedText = HtmlCompat.fromHtml(htmlText, HtmlCompat.FROM_HTML_MODE_LEGACY)
+            binding.tvAgreementContents.text = spannedText
+
+            val currentDate = Date()
+            val signatureDate = dateDateToString(currentDate)
+            binding.tvAgreementCurrentDate.text = signatureDate
+        } catch (e: FileNotFoundException) {
+            Log.e("MypageAgreementPrivacy", "file not found")
+            binding.tvAgreementContents.text = getString(R.string.msg_not_found_file)
         }
     }
 }
