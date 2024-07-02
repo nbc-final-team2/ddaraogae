d5580ed8e877e0cd36b9e3a0971cf5e9a1eb74c6
neoneoneo123 <gouxrunner@gmail.com>
2024-07-02 13:58:24 +0900

    feat: 개인정보처리 약관 html text로 읽도록 처리함
    issue:269

--- app/src/main/java/com/nbcfinalteam2/ddaraogae/presentation/ui/mypage/MypagePrivacyActivity.kt
+++ app/src/main/java/com/nbcfinalteam2/ddaraogae/presentation/ui/mypage/MypagePrivacyActivity.kt
@@ -1,14 +1,16 @@
 package com.nbcfinalteam2.ddaraogae.presentation.ui.mypage
 
 import android.os.Bundle
-import android.text.Html
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.text.HtmlCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updatePadding
 import com.nbcfinalteam2.ddaraogae.R
 import com.nbcfinalteam2.ddaraogae.databinding.ActivityPrivacyBinding
+import java.io.FileNotFoundException
 
 class MypagePrivacyActivity : AppCompatActivity() {
     private lateinit var binding: ActivityPrivacyBinding
@@ -27,6 +29,16 @@ class MypagePrivacyActivity : AppCompatActivity() {
             val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
             view.updatePadding(insets.left, insets.top, insets.right, insets.bottom)
             WindowInsetsCompat.CONSUMED
+        }
+
+        try {
+            val assetManager = this.assets.open("doc_privacy_rules")
+            val htmlText = assetManager.bufferedReader().use { it.readText() }
+            val spannedText = HtmlCompat.fromHtml(htmlText, HtmlCompat.FROM_HTML_MODE_LEGACY)
+            binding.tvPrivacyContents.text = spannedText
+        } catch (e: FileNotFoundException) {
+            Log.e("MypagePrivacy", "file not found")
+            binding.tvPrivacyContents.text = getString(R.string.msg_not_found_file)
         }
     }
 }
