1bdf0691dacf7287be6c878f668e82382bf07118
Lee HuiGwang <lhk9805@gmail.com>
2024-07-04 11:56:42 +0900

    fix: 충돌 해결
    issue: #

--- app/src/main/java/com/nbcfinalteam2/ddaraogae/presentation/ui/dog/DetailPetFragment.kt
+++ app/src/main/java/com/nbcfinalteam2/ddaraogae/presentation/ui/dog/DetailPetFragment.kt
@@ -1,12 +1,10 @@
 package com.nbcfinalteam2.ddaraogae.presentation.ui.dog
 
 import android.content.Intent
-import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
 import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -15,11 +13,9 @@ import androidx.lifecycle.lifecycleScope
 import com.bumptech.glide.Glide
 import com.nbcfinalteam2.ddaraogae.R
 import com.nbcfinalteam2.ddaraogae.databinding.FragmentDetailPetBinding
-import com.nbcfinalteam2.ddaraogae.presentation.model.DefaultEvent
 import com.nbcfinalteam2.ddaraogae.presentation.ui.edit.EditPetActivity.Companion.DOGDATA
 import com.nbcfinalteam2.ddaraogae.presentation.model.DogInfo
 import com.nbcfinalteam2.ddaraogae.presentation.ui.edit.EditPetActivity
-import com.nbcfinalteam2.ddaraogae.presentation.util.ToastMaker
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
