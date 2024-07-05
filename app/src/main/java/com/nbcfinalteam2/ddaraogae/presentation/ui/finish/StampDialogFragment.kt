28983d31c423fa35843ba4e9dcd272baf0fcc5b9
neoneoneo123 <gouxrunner@gmail.com>
2024-07-05 11:19:37 +0900

    fix: 스탬프 획득 화면 크기 개선 - width를 기기 크게에 맞춰 동적으로 처리
    issue: #304

--- app/src/main/java/com/nbcfinalteam2/ddaraogae/presentation/ui/finish/StampDialogFragment.kt
+++ app/src/main/java/com/nbcfinalteam2/ddaraogae/presentation/ui/finish/StampDialogFragment.kt
@@ -1,14 +1,16 @@
 package com.nbcfinalteam2.ddaraogae.presentation.ui.finish
 
+import android.content.Context
 import android.graphics.Color
+import android.graphics.Point
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.Window
+import android.view.WindowManager
 import androidx.fragment.app.DialogFragment
-import com.nbcfinalteam2.ddaraogae.R
 import com.nbcfinalteam2.ddaraogae.databinding.FragmentStampDialogBinding
 import com.nbcfinalteam2.ddaraogae.domain.entity.StampEntity
 import dagger.hilt.android.AndroidEntryPoint
@@ -34,14 +36,34 @@ class StampDialogFragment : DialogFragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         _binding = FragmentStampDialogBinding.inflate(inflater, container, false)
+
+        setDialogUI()
+
+        return binding.root
+    }
+
+    private fun setDialogUI() {
+        val windowManager = requireActivity().getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        val display = windowManager.defaultDisplay
+        val size = Point()
+        display.getSize(size)
+
+        val params: ViewGroup.LayoutParams? = dialog?.window?.attributes
+        val deviceWidth = size.x
+        params?.width = (deviceWidth * 0.8).toInt()
+        dialog?.window?.attributes = params as WindowManager.LayoutParams
+
         dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         dialog?.window?.requestFeature(Window.FEATURE_NO_TITLE)
-        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initView()
+    }
+
+    private fun initView() {
         val stampList: ArrayList<StampEntity>? = arguments?.getParcelableArrayList(ARG_STAMP_LIST)
 
         val adapter = stampList?.let { FinishStampViewPagerAdapter(it) }
