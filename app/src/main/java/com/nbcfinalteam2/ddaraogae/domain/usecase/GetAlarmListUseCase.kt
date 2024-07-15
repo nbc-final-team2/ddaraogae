c32c0fdd76d7e4f04d806beb42968a494136cd0a
Lee HuiGwang <lhk9805@gmail.com>
2024-07-16 04:00:09 +0900

    remove: 사용되지 않는 불필요 코드 제거
    issue: #303

--- app/src/main/java/com/nbcfinalteam2/ddaraogae/domain/usecase/GetAlarmListUseCase.kt
+++
@@ -1,8 +0,0 @@
-package com.nbcfinalteam2.ddaraogae.domain.usecase
-
-import com.nbcfinalteam2.ddaraogae.domain.entity.AlarmEntity
-import kotlinx.coroutines.flow.Flow
-
-interface GetAlarmListUseCase {
-    operator fun invoke(): Flow<List<AlarmEntity>>
-}
--- app/src/main/java/com/nbcfinalteam2/ddaraogae/domain/usecase/UpdateAlarmUseCase.kt
+++
@@ -1,7 +0,0 @@
-package com.nbcfinalteam2.ddaraogae.domain.usecase
-
-import com.nbcfinalteam2.ddaraogae.domain.entity.AlarmEntity
-
-interface UpdateAlarmUseCase {
-    suspend operator fun invoke(alarmEntity: AlarmEntity)
-}
